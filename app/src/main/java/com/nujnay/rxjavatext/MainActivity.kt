18f3f2337ed1cec5964eabd153db8c43c1e3512e
8937 <yanjun@adpanshi.com>
2018-12-29 17:26:05 +0800

    ···

--- app/src/main/java/com/nujnay/rxjavatext/MainActivity.kt
+++ app/src/main/java/com/nujnay/rxjavatext/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        Flowable
+//                .intervalRange(0, 10, 0, 1, TimeUnit.SECONDS)
+
     }
 }
