a0d9ad18d74e6d6b267c25fc3bf76e7702e7a444
yanjun <1292015395@qq.com>
2020-07-20 15:19:58 +0800

    master

--- app/src/main/java/com/nujnay/rxjavatext/MainActivity.kt
+++ app/src/main/java/com/nujnay/rxjavatext/MainActivity.kt
@@ -3,7 +3,7 @@ package com.nujnay.rxjavatext
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import io.reactivex.Flowable
+import io.reactivex.*
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import java.text.SimpleDateFormat
@@ -14,21 +14,51 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val disposable = Flowable
-            .interval(2, TimeUnit.SECONDS)
-            .map {
-                Log.d("timetime:11::", formateDataStandard(Date(System.currentTimeMillis())))
-            }
-            .flatMap {
-                return@flatMap Flowable.just("1").delay(2,TimeUnit.SECONDS)
-            }
-            .subscribeOn(Schedulers.io())
-            .subscribe({
-                Log.d("timetime:22::",it+"||"+ formateDataStandard(Date(System.currentTimeMillis())))
-            }, {
-
-            })
+//        var a = 1
+//
+//        setContentView(R.layout.activity_main)
+//        val disposable = Flowable
+//            .interval(3, TimeUnit.SECONDS)
+//            .map {
+//                Log.d("timetime:11::", formateDataStandard(Date(System.currentTimeMillis())))
+//            }
+//            .flatMap {
+//                return@flatMap Flowable.create(
+//                    { e: FlowableEmitter<Int> ->
+//                        Log.d(
+//                            "timetime:22::",
+//                            formateDataStandard(Date(System.currentTimeMillis()))
+//                        )
+//                        e.onNext(a++)
+//                    },
+//                    BackpressureStrategy.BUFFER
+//                ).delay(3, TimeUnit.SECONDS)
+//            }
+//            .subscribeOn(Schedulers.io())
+//            .takeWhile {
+//                if (it > 4) {
+//                    return@takeWhile false
+//                }
+//                Log.d("timetime:33::", formateDataStandard(Date(System.currentTimeMillis())))
+//                return@takeWhile true
+//            }
+//            .subscribe({
+//                Log.d(
+//                    "timetime:44::",
+//                    it.toString() + "||" + formateDataStandard(Date(System.currentTimeMillis()))
+//                )
+//                Thread.sleep(4000)
+//                Log.d(
+//                    "timetime:55::",
+//                    it.toString() + "||" + formateDataStandard(Date(System.currentTimeMillis()))
+//                )
+//            }, {
+//
+//            }, {
+//                Log.d(
+//                    "timetime:66::", formateDataStandard(Date(System.currentTimeMillis()))
+//                )
+//            })
 
     }
 }
